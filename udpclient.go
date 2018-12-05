94ba8352752f236cc42e1c56cb3a609c02eb4535
Gustavo Maurizio <gustavo.maurizio@gmail.com>
2018-12-04 19:49:57 -0500

    Tue Dec  4 19:49:57 EST 2018

---
+++ udpclient.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s host:port", os.Args[0])
+		os.Exit(1)
+	}
+	service := os.Args[1]
+
+	udpAddr, err := net.ResolveUDPAddr("udp", service)
+	checkError(err)
+
+	conn, err := net.DialUDP("udp", nil, udpAddr)
+	checkError(err)
+
+	var bufSend []byte
+	bufSend = make([]byte, 1024*5)
+
+	_, err = conn.Write(bufSend)
+	checkError(err)
+	/*
+		var buf [512]byte
+		n, err := conn.Read(buf[0:])
+		checkError(err)
+
+		fmt.Println(string(buf[0:n]))
+	*/
+	os.Exit(0)
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error %v", err)
+		os.Exit(1)
+	}
+}
