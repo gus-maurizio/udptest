df7f82db1ad6c4b3f75f98afa59f03740c52e425
gustavo maurizio <gustavo,maurizio@gmail.com>
2018-12-05 20:23:21 -0500

    Wed Dec  5 20:23:21 EST 2018

--- udpmacsender.go
+++ udpmacsender.go
@@ -1,20 +1,17 @@
 package main
-  
+
+
 import (
-        "log"
-        "net"
-        "os"
-        "strconv"
-        "syscall"
-        "time"
-        "unsafe"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"syscall"
+	"time"
 )
 
-// #include <sys/syscall.h>
-import "C"
-
 const (
-	payload_sz = 32
+	payload_sz = 512
 	chunk_sz   = 1024
 	local_port = 5674
 )
@@ -40,8 +37,6 @@ func main() {
 		sendTo(addr, iter)
 	case "sendMsg":
 		sendMsg(addr, iter)
-	case "sendMMsg":
-		sendMMsg(addr, iter)
 	case "All":
 		write(addr, iter)
 		time.Sleep(time.Second * 2)
@@ -50,15 +45,13 @@ func main() {
 		sendTo(addr, iter)
 		time.Sleep(time.Second * 2)
 		sendMsg(addr, iter)
-		time.Sleep(time.Second * 2)
-		sendMMsg(addr, iter)
 	default:
 		usage()
 	}
 }
 
 func usage() {
-	log.Fatal("Usage: %s write|writeToUDP|sendTo|sendMsg|sendMMsg|All  iteration_count*1M  target_ip:port", os.Args[0])
+	log.Fatal("Usage: %s write|writeToUDP|sendTo|sendMsg|All  iteration_count*1M  target_ip:port", os.Args[0])
 }
 
 func chk(err error) {
@@ -128,42 +121,6 @@ type MMsghdr struct {
 	cnt int
 }
 
-func sendMMsg(addr *net.UDPAddr, i int) {
-	i = i / chunk_sz
-	log.Printf("Start `sendMMsg` test with %d iteration\n", i)
-
-	laddr := UDPAddrToSockaddr(&net.UDPAddr{Port: local_port, IP: net.IPv4zero})
-	raddr := UDPAddrToSockaddr(addr)
-
-	fd := connectUDP(laddr, raddr)
-
-	msgcnt := chunk_sz
-	var msgArr [chunk_sz]MMsghdr
-	for j := 0; j < msgcnt; j++ {
-		p := make([]byte, payload_sz)
-		for k := 0; k < payload_sz; k++ {
-			p[k] = byte(k)
-		}
-
-		var iov syscall.Iovec
-		iov.Base = (*byte)(unsafe.Pointer(&p[0]))
-		iov.SetLen(len(p))
-
-		var msg syscall.Msghdr
-		msg.Iov = &iov
-		msg.Iovlen = 1
-
-		msgArr[j] = MMsghdr{msg, 0}
-	}
-
-	for ; i > 0; i-- {
-		//_, _, e1 := syscall.Syscall6(C.SYS_sendmmsg, uintptr(fd), uintptr(unsafe.Pointer(&msgArr[0])), uintptr(msgcnt), uintptr(syscall.MSG_DONTWAIT), 0, 0)
-		_, _, e1 := syscall.Syscall6(C.SYS_sendmmsg, uintptr(fd), uintptr(unsafe.Pointer(&msgArr[0])), uintptr(msgcnt), 0, 0, 0)
-		if e1 != 0 {
-			panic("error on sendmmsg")
-		}
-	}
-}
 
 func UDPAddrToSockaddr(addr *net.UDPAddr) *syscall.SockaddrInet4 {
 	raddr := &syscall.SockaddrInet4{Port: addr.Port, Addr: [4]byte{addr.IP[12], addr.IP[13], addr.IP[14], addr.IP[15]}}
